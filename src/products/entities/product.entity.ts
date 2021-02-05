2961ed86fe8661a3a591a9bacaac84244a9012de
unknown <monicaiyb@gmail.com>
2021-02-05 16:02:57 +0300

    Add Product resource

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+export class Product {
+@PrimaryGeneratedColumn()
+id: number;
+
+@Column({ nullable: false })
+pname: string;
+
+@Column({ nullable: false })
+ward: string;
+
+@Column({ nullable: true })
+dateOfUpload: string;
+
+@Column({ nullable: false })
+farmername: string;
+
+@Column({ nullable: true })
+image: string;
+}
